f8311bb00019a77fe21848a070e9d521ac1e3c83
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-05-30 20:54:29 -0400

    feat: add section

--- src/pages/books/index.jsx
+++ src/pages/books/index.jsx
@@ -3,12 +3,14 @@ import MediaTable from '@/components/MediaTable'
 import { Box, Heading } from '@chakra-ui/react'
 
 export default function Books() {
+  const mediaTypes = ['books']
+
   return (
     <Box p={4}>
       <Heading as="h1" size="xl">
         Welcome to Chakra UI!
       </Heading>
-      <MediaTable />
+      <MediaTable mediaType="books" />
     </Box>
   )
 }
