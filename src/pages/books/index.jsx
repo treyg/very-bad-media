4d77208e01d253aa058399778d4bc05fc562f81e
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-05-31 21:07:09 -0400

    feat :add content to index

--- src/pages/books/index.jsx
+++ src/pages/books/index.jsx
@@ -1,16 +1,51 @@
 import MediaTable from '@/components/MediaTable'
+import { Box, Heading, Text } from '@chakra-ui/react'
 
-import { Box, Heading } from '@chakra-ui/react'
+export default function Books({ episodes }) {
+  const selectedMedia = ['books', 'shortStories']
 
-export default function Books() {
-  const mediaTypes = ['books']
+  const headings = {
+    books: 'Books',
+    shortStories: 'Short Stories',
+    movies: 'Movies',
+    tvshows: 'TV Shows',
+    articles: 'Articles',
+    essays: 'Essays'
+  }
+
+  function generateHeading(selectedMedia) {
+    if (selectedMedia.length === 1) {
+      return headings[selectedMedia[0]]
+    } else {
+      return selectedMedia.map(type => headings[type]).join(' and ')
+    }
+  }
 
   return (
     <Box p={4}>
-      <Heading as="h1" size="xl">
-        Welcome to Chakra UI!
+      <Heading as="h1" size="xl" mt="20px">
+        {generateHeading(selectedMedia)}
+        <Text as="p" fontSize="sm" fontWeight="normal" my="10px">
+          {generateHeading(selectedMedia)} discussed on the Very Bad Wizards podcast.
+        </Text>
       </Heading>
-      <MediaTable mediaType="books" />
+
+      <MediaTable episodes={episodes} mediaTypes={selectedMedia} />
     </Box>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch(`http://localhost:3000/api/data`, {
+    headers: {
+      'Cache-Control': 'no-cache'
+    }
+  })
+  const episodes = await response.json()
+
+  return {
+    props: {
+      episodes
+    }
+  }
 }
