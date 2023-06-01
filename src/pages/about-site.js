bd2451fe90764695085c85f254f964979e4b04cc
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-06-01 18:24:32 -0400

    bug: update text analysis

---
+++ src/pages/about-site.js
@@ -0,0 +1,8 @@
+import AboutPage from '../components/AboutPage'
+import aboutSiteInfo from '../data/about-site.json'
+
+const AboutSite = () => {
+  return <AboutPage pageInfo={aboutSiteInfo} />
+}
+
+export default AboutSite
--- src/pages/api/textAnalysis.js
+++ src/pages/api/textAnalysis.js
@@ -17,28 +17,31 @@ async function analyzeText(text) {
   const conversation = [
     {
       role: 'system',
-      content: `You are a helpful assistant that parses text and identifies references to movies/films, TV shows, books, short stories, articles, essays. You should respond with nothing other than YAML. Organize the identified references into separate lists, each keyed by category in the response. Include the episode title and link in the response. Follow this exact format for the response. Don't include anything else. If you aren't sure if something is an article or an essay, just put it in the articles list. Make sure you return only valid YAML. Don't include any other text in the response and only include categories that are explicitly mentioned in the following example.
- 
-       Example format:
-       movies:
-         - "John Wick"
-         - "The Lion King"
-       tvshows:
-         - "The Office"
-         - "The Simpsons"
-       books:
-         - title: "The Hobbit"
-           author: "J.R.R. Tolkien"
-       shortStories:
-         - title: "The Last Question"
-           author: "Isaac Asimov"
-       articles:
-         - title: "The AI Revolution: The Road to Superintelligence - Tim Urban"
-           link: "https://waitbutwhy.com/2015/01/artificial-intelligence-revolution-1.html"
-       essays:
-         - title: "This is Water"
-           author: "David Foster Wallace"
-     `
+      content: `You are a helpful assistant that parses text and identifies references to movies/films, TV shows, books, short stories, articles, essays. If the author for a book isn't directly mentioned, but you know who it is, feel free to add it. You should respond with nothing other than YAML. Organize the identified references into separate lists, each keyed by category in the response. Include the episode title and link in the response. Follow this exact format for the response. If you aren't sure if something is an article or an essay, just put it in the articles list. Make sure you return only valid YAML. Only include categories that are explicitly mentioned in the following example format. Remember, each category should appear only once in the mapping. The links for movies should be to letterboxd.com. Article links are found in the text being analyzed. Tv show, books, and short story links are not necessary.
+
+        Example format:
+        movies:
+          - title: "John Wick"
+            director: "Chad Stahelski"
+            link: 'https://letterboxd.com/film/john-wick/'
+        tvshows:
+          - title: "The office"
+            director: "Greg Daniels"
+        books:
+          - title: "The Hobbit"
+            author: "J.R.R. Tolkien"
+        shortstories:
+          - title: "The Last Question"
+            author: "Isaac Asimov"
+        articles:
+          - title: "The AI Revolution: The Road to Superintelligence - Tim Urban"
+            link: "https://waitbutwhy.com/2015/01/artificial-intelligence-revolution-1.html"
+          - title: "Another Article"
+            link: "https://another.link"
+        essays:
+          - title: "This is Water"
+            author: "David Foster Wallace"
+      `
     },
     { role: 'user', content: `Text: ${text}` }
   ]
@@ -46,14 +49,44 @@ async function analyzeText(text) {
   const result = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: conversation,
-    max_tokens: 2500,
-    temperature: 0.2
+    max_tokens: 2300,
+    temperature: 0.3
   })
 
-  console.log('Text analyzed successfully!')
-  const yamlData = yaml.load(result.data.choices[0].message.content)
-  const jsonData = JSON.stringify(yamlData)
-  return jsonData
+  console.log('Text analysis complete!')
+  try {
+    const yamlData = yaml.load(result.data.choices[0].message.content)
+    const categories = [
+      'movies',
+      'tvshows',
+      'books',
+      'shortstories',
+      'articles',
+      'essays'
+    ]
+
+    // Create a new object to hold the merged lists
+    let mergedData = {}
+
+    // Iterate over each category
+    for (let category of categories) {
+      // Check if the category exists in the data
+      if (yamlData[category]) {
+        // If the category already exists in the mergedData, append the new items
+        if (mergedData[category]) {
+          mergedData[category] = [...mergedData[category], ...yamlData[category]]
+        } else {
+          // If the category does not exist in the mergedData, add it
+          mergedData[category] = yamlData[category]
+        }
+      }
+    }
+
+    const jsonData = JSON.stringify(mergedData)
+    return jsonData
+  } catch (error) {
+    console.error('Error parsing YAML:', error)
+  }
 }
 
 export default analyzeText
