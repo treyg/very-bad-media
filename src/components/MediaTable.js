3254bd3fab07dba3b33f355b8ec97ae3b49af360
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-05-31 21:07:49 -0400

    feat: allow multiple types on table

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,67 +1,76 @@
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react'
+import { Table, Thead, Tbody, Tr, Th, Td, Text } from '@chakra-ui/react'
 
 function formatDate(dateString) {
-  const options = { year: 'numeric', month: 'short', day: 'numeric' }
+  const options = { year: 'numeric', month: 'numeric', day: 'numeric' }
   const date = new Date(dateString)
   return date.toLocaleDateString(undefined, options)
 }
 
-const MediaTable = ({ mediaType }) => {
-  const router = useRouter()
-  const [episodes, setEpisodes] = useState([])
+const typeFixes = {
+  movies: 'Movie',
+  tvshows: 'TV Show',
+  books: 'Book',
+  articles: 'Article',
+  essays: 'Essay',
+  shortStories: 'Short Story'
+}
 
-  useEffect(() => {
-    async function fetchMediaData() {
-      const response = await fetch(`http://localhost:3000/api/data`, {
-        headers: {
-          'Cache-Control': 'no-cache'
-        }
-      })
-      const media = await response.json()
-      setEpisodes(media)
-    }
+function fixType(type) {
+  return typeFixes[type] || type
+}
 
-    fetchMediaData()
-  }, [])
-
-  const data = episodes.map(episode => ({
-    episodeTitle: episode.episode,
-    episodeLink: episode.link,
-    episodeDate: formatDate(episode.pubDate),
-    media: episode.mediaList[mediaType] || [] // Access mediaList[mediaType] and provide an empty array as a fallback
-  }))
+const MediaTable = ({ episodes, mediaTypes }) => {
+  const data = episodes
+    ? episodes
+        .map(episode =>
+          mediaTypes.map(type => ({
+            episodeTitle: episode.episode,
+            episodeLink: episode.link,
+            episodeDate: formatDate(episode.pubDate),
+            mediaType: type, // Use 'type' instead of 'mediaType'
+            media: episode.mediaList[type] || [] // Access mediaList[type] and provide an empty array as a fallback
+          }))
+        )
+        .reduce((acc, val) => acc.concat(val), [])
+    : []
 
   return (
-    <div className="overflow-x-auto">
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Title</Th>
-            <Th>Author</Th>
-            <Th>Date</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data.map((item, index) =>
-            item.media.map((mediaItem, mIndex) => (
-              <Tr key={`${index}-${mIndex}`} className={index % 2 === 0 ? 'active' : ''}>
-                <Td>
-                  {mediaItem.title}
-                  <br />
-                  <a href={item.episodeLink} className="text-sm">
-                    {item.episodeTitle}
-                  </a>
-                </Td>
-                <Td>{mediaItem.author || mediaItem.director}</Td>
-                <Td>{item.episodeDate}</Td>
-              </Tr>
-            ))
-          )}
-        </Tbody>
-      </Table>
-    </div>
+    <Table variant="simple">
+      <Thead>
+        <Tr>
+          <Th>Title</Th>
+          <Th>Type</Th>
+          <Th>Author</Th>
+          <Th>Date</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {data.map((item, index) =>
+          item.media.map((mediaItem, mIndex) => (
+            <Tr key={`${index}-${mIndex}`} className={index % 2 === 0 ? 'active' : ''}>
+              <Td>
+                <Text fontSize="md" fontWeight="bold">
+                  {typeof mediaItem === 'string' ? mediaItem : mediaItem.title}
+                </Text>
+                <Text fontSize="sm" color="gray.500">
+                  <a href={item.episodeLink}>{item.episodeTitle}</a>
+                </Text>
+              </Td>
+              <Td>
+                <Text fontSize="md">{fixType(item.mediaType)}</Text>
+              </Td>
+              <Td>
+                <Text fontSize="sm">{mediaItem.author || mediaItem.director || ''}</Text>
+              </Td>
+              <Td>
+                <Text fontSize="sm">{item.episodeDate}</Text>
+              </Td>
+            </Tr>
+          ))
+        )}
+      </Tbody>
+    </Table>
   )
 }
 
