5cb4144ed148de4d3a827d921613968c4b46f345
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-05-31 21:05:57 -0400

    chore: update nav data

--- src/components/WithSubNavigation.js
+++ src/components/WithSubNavigation.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import NAV_ITEMS from '@/data/NavItems'
 import {
   Box,
   Flex,
@@ -236,44 +237,3 @@ const MobileNavItem = ({ label, children, href }) => {
     </Stack>
   )
 }
-
-const NAV_ITEMS = [
-  {
-    label: 'Inspiration',
-    children: [
-      {
-        label: 'Explore Design Work',
-        subLabel: 'Trending Design to inspire you',
-        href: '#'
-      },
-      {
-        label: 'New & Noteworthy',
-        subLabel: 'Up-and-coming Designers',
-        href: '#'
-      }
-    ]
-  },
-  {
-    label: 'Find Work',
-    children: [
-      {
-        label: 'Job Board',
-        subLabel: 'Find your dream design job',
-        href: '#'
-      },
-      {
-        label: 'Freelance Projects',
-        subLabel: 'An exclusive list for contract work',
-        href: '#'
-      }
-    ]
-  },
-  {
-    label: 'Learn Design',
-    href: '#'
-  },
-  {
-    label: 'Hire Designers',
-    href: '#'
-  }
-]
---
+++ src/data/NavItems.js
@@ -0,0 +1,52 @@
+const NAV_ITEMS = [
+  {
+    label: 'Media',
+    children: [
+      {
+        label: 'Books',
+        subLabel: 'Books list',
+        href: '/books'
+      },
+      {
+        label: 'Short Stories',
+        subLabel: 'Short Stories list',
+        href: '/short-stories'
+      },
+      {
+        label: 'Movies',
+        subLabel: 'Movies list',
+        href: '/movies'
+      },
+      {
+        label: 'TV Shows',
+        subLabel: 'TV Shows list',
+        href: '/tvshows'
+      }
+    ]
+  },
+  {
+    label: 'Find Work',
+    children: [
+      {
+        label: 'Job Board',
+        subLabel: 'Find your dream design job',
+        href: '#'
+      },
+      {
+        label: 'Freelance Projects',
+        subLabel: 'An exclusive list for contract work',
+        href: '#'
+      }
+    ]
+  },
+  {
+    label: 'Podcast',
+    href: '#'
+  },
+  {
+    label: 'About',
+    href: '#'
+  }
+]
+
+export default NAV_ITEMS
