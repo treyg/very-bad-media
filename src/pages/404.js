e13d3a10f08bb8e2882617da3b76d6771a7eb297
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-06-01 20:00:21 -0400

    feat: add simple 404

---
+++ src/pages/404.js
@@ -0,0 +1,34 @@
+import { Box, Heading, Text, Button, useColorModeValue } from '@chakra-ui/react'
+
+const NotFoundPage = () => {
+  const buttonColor = useColorModeValue('light.primary', 'dark.primary')
+  const buttonHoverColor = useColorModeValue('green.500', 'green.500')
+
+  return (
+    <Box textAlign="center" my="60">
+      <Heading as="h1" size="2xl" mb="2">
+        404
+      </Heading>
+      <Heading as="h2" size="xl" mb="2">
+        Page Not Found
+      </Heading>
+      <Text fontSize="xl" mb="4">
+        The page you were looking for doesn't exist.
+      </Text>
+      <Button
+        as="a"
+        href="/"
+        colorScheme={buttonColor} // Set the button color scheme
+        bg={buttonColor}
+        px={6}
+        _hover={{
+          bg: buttonHoverColor
+        }}
+      >
+        Go Home
+      </Button>
+    </Box>
+  )
+}
+
+export default NotFoundPage
