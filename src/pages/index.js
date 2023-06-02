27d97a79247af729f0ac58040c3f1908751c883e
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-06-02 11:42:49 -0400

    bug: fix api route

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,12 +49,13 @@ export default function Home({ episodes }) {
 }
 
 export async function getServerSideProps() {
-  const response = await fetch('/api/data')
+  const response = await fetch('http://localhost:3000/api/data')
   const episodes = await response.json()
 
   return {
     props: {
       episodes
+      //isLoading: false
     }
   }
 }
