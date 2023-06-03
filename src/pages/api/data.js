15ae4a37316eaeef22301356b40d8d86b7c40882
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-06-02 21:36:30 -0400

    chore: update error handling

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -11,7 +11,17 @@ export default async function handler(req, res) {
       const collection = client.db(process.env.MONGODB_DB).collection('episodes')
 
       // Fetch all documents from the collection
-      const episodesData = await collection.find().toArray()
+      let episodesData
+      try {
+        episodesData = await collection.find().toArray()
+        console.log(episodesData) // Log the data returned from MongoDB
+      } catch (error) {
+        console.error('Error fetching data from MongoDB:', error)
+        res
+          .status(500)
+          .json({ message: 'Error fetching data from MongoDB', error: error.toString() })
+        return
+      }
 
       // Close the connection to MongoDB
       await client.close()
