facef729d41f97568f8d5c1751f80810783db830
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-06-03 19:39:01 -0400

    feat: add coffee link

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,7 +1,8 @@
-import { Box, Container, Heading, Text } from '@chakra-ui/react'
+import { Box, Container, Heading, Text, Link } from '@chakra-ui/react'
+import Image from 'next/image'
 
 const AboutPage = ({ pageInfo }) => {
-  const { title, description } = pageInfo
+  const { title, description, coffeeLink } = pageInfo
 
   return (
     <Container maxW="container.md" py={8}>
@@ -9,7 +10,14 @@ const AboutPage = ({ pageInfo }) => {
         <Heading as="h1" size="xl" mb={4}>
           {title}
         </Heading>
-        <Text fontSize="lg">{description}</Text>
+        <Text mb={20} fontSize="lg">
+          {description}
+        </Text>
+        {coffeeLink && (
+          <Link href={coffeeLink} isExternal>
+            <Image src="/coffee.svg" alt="Buy me a coffee" width={150} height={150} />
+          </Link>
+        )}
       </Box>
     </Container>
   )
