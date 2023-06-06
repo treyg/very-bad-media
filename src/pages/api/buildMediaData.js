18a1106a11fea146e53013b2319386e191c44883
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-06-05 20:28:16 -0400

    chore: refactor db code

--- src/pages/api/buildMediaData.js
+++ src/pages/api/buildMediaData.js
@@ -1,16 +1,14 @@
-import { MongoClient } from 'mongodb'
 import fetchRss from './fetchRss.js'
 import analyzeText from './textAnalysis.js'
+import {
+  connectToDb,
+  checkExistingEpisode,
+  insertEpisodeData,
+  closeDbConnection
+} from './mongoDb.js'
 
 export async function buildData(rssFeedUrl) {
-  console.log('Connecting to MongoDB...')
-  // Connect to MongoDB
-  const client = new MongoClient(process.env.MONGODB_URI)
-  await client.connect()
-  console.log('Connected to MongoDB.')
-
-  // Get a reference to the collection where you'll store the data
-  const collection = client.db(process.env.MONGODB_DB).collection('episodes')
+  const { client, collection } = await connectToDb()
 
   console.log('Fetching RSS feed...')
   const episodes = await fetchRss(rssFeedUrl)
@@ -20,8 +18,7 @@ export async function buildData(rssFeedUrl) {
     // Use the episode guid as the key
     const key = `episode:${episode.guid}`
 
-    // Check if a document with this guid already exists
-    const existingEpisode = await collection.findOne({ guid: episode.guid })
+    const existingEpisode = await checkExistingEpisode(collection, episode.guid)
     if (existingEpisode) {
       console.log(`Episode ${key} already exists in MongoDB, skipping...`)
       continue
@@ -52,22 +49,16 @@ export async function buildData(rssFeedUrl) {
       link: episode.link,
       guid: episode.guid,
       pubDate: episode.pubDate,
-      mediaList: parsedMediaList // Assign parsed mediaList back to property
+      mediaList: parsedMediaList
     }
 
-    // Store the episode data in MongoDB
-    console.log(`Storing data for episode ${key} in MongoDB...`)
-    await collection.insertOne(episodeData)
-    console.log(`Stored data for episode ${key} in MongoDB.`)
+    await insertEpisodeData(collection, episodeData)
 
     // Wait for 1 second before the next iteration
     await new Promise(resolve => setTimeout(resolve, 3000))
   }
 
-  // Close the connection to MongoDB
-  console.log('Closing connection to MongoDB...')
-  await client.close()
-  console.log('Closed connection to MongoDB.')
+  await closeDbConnection(client)
 }
 
 buildData('http://feeds.libsyn.com/474285/rss')
---
+++ src/pages/api/mongoDb.js
@@ -0,0 +1,28 @@
+// mongoDb.js
+import { MongoClient } from 'mongodb'
+
+export async function connectToDb() {
+  console.log('Connecting to MongoDB...')
+  const client = new MongoClient(process.env.MONGODB_URI)
+  await client.connect()
+  console.log('Connected to MongoDB.')
+
+  const collection = client.db(process.env.MONGODB_DB).collection('episodes')
+  return { client, collection }
+}
+
+export async function checkExistingEpisode(collection, guid) {
+  return await collection.findOne({ guid: guid })
+}
+
+export async function insertEpisodeData(collection, episodeData) {
+  console.log(`Storing data for episode ${episodeData.guid} in MongoDB...`)
+  await collection.insertOne(episodeData)
+  console.log(`Stored data for episode ${episodeData.guid} in MongoDB.`)
+}
+
+export async function closeDbConnection(client) {
+  console.log('Closing connection to MongoDB...')
+  await client.close()
+  console.log('Closed connection to MongoDB.')
+}
