c31b5f1709cce30fca0b84e82eecc19adab40fb4
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-06-12 20:34:11 -0400

    feat: update about page

--- src/pages/about-podcast.js
+++ src/pages/about-podcast.js
@@ -1,8 +1,56 @@
-import AboutPage from '../components/AboutPage'
-import aboutPodcastInfo from '../data/about-podcast.json'
+import { Box, Container, Heading, Text, Link, Grid } from '@chakra-ui/react'
+import Image from 'next/image'
+import SocialProfile from '../components/SocialProfile'
 
 const AboutPodcast = () => {
-  return <AboutPage pageInfo={aboutPodcastInfo} />
+  const hosts = [
+    {
+      name: 'David Pizarro',
+      handle: '@peez',
+      twitterUrl: 'https://twitter.com/peez',
+      avatar: '/dave.jpeg',
+      bio: 'David Pizarro is a professor at Cornell University, specializing in moral judgement and emotion.',
+      hashtags: []
+    },
+    {
+      name: 'Tamler Sommers',
+      handle: '@tamler',
+      twitterUrl: 'https://twitter.com/tamler',
+      avatar: '/tamler.jpeg',
+      bio: 'Tamler Sommers is a professor at the University of Houston, specializing in honor and morality.',
+      hashtags: []
+    }
+  ]
+
+  return (
+    <Container maxW="container.md" py={8}>
+      <Box textAlign="center">
+        <Heading as="h1" size="xl" mb={4}>
+          About Very Bad Wizards
+        </Heading>
+        <Text mb={20} fontSize="lg">
+          Very Bad Wizards is a podcast featuring a philosopher (Tamler Sommers) and a
+          psychologist (David Pizarro), who share a love for ethics, pop culture, and
+          cognitive science, and who have a marked inability to distinguish sacred from
+          profane. Each podcast includes discussions of moral philosophy, recent work on
+          moral psychology and neuroscience, and the overlap between the two.
+        </Text>
+        <Grid templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }} gap={6}>
+          {hosts.map((host, index) => (
+            <SocialProfile
+              key={index}
+              name={host.name}
+              twitterUrl={host.twitterUrl}
+              handle={host.handle}
+              avatar={host.avatar}
+              bio={host.bio}
+              hashtags={host.hashtags}
+            />
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  )
 }
 
 export default AboutPodcast
