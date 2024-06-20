3a656a56d92440bedd243e0ce151ffa8ad55ed85
Trey Gordon <41927921+treyg@users.noreply.github.com>
2024-06-20 18:58:01 -0400

    bug: fix static props pattern + update head script

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-
+import Script from "next/script";
+import { MongoClient } from "mongodb";
 import CallToActionWithAnnotation from "@/components/CallToActionAnnotation";
 import DottedBox from "@/components/DottedBox";
 import BackgroundBlobs from "@/components/BackgroundBlobs";
@@ -17,11 +18,11 @@ export default function Home({ episodes }) {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <script
+        <Script
           defer
           data-domain="verybad.media"
           src="https://plausible.tagweb.dev/js/script.js"
-        ></script>
+        />
       </Head>
 
       <Box position="relative">
@@ -51,35 +52,40 @@ export default function Home({ episodes }) {
 }
 
 export async function getStaticProps() {
-  const apiUrl = process.env.API_URL || "http://localhost:3000/api/data";
-  console.log("mong", process.env.MONGODB_URI);
-  const response = await fetch(apiUrl);
+  console.log("Connecting to MongoDB...");
+  const client = new MongoClient(process.env.MONGODB_URI);
 
-  if (!response.ok) {
-    throw new Error(`API request failed with status ${response.status}`);
-  }
-
-  const responseText = await response.text();
-
-  const contentType = response.headers.get("Content-Type");
-  if (!contentType || !contentType.includes("application/json")) {
-    console.error("Unexpected content type:", contentType);
-    console.error("Raw response text:", responseText);
-    throw new Error(`Unexpected content type: ${contentType}`);
-  }
-
-  let episodes;
   try {
-    episodes = JSON.parse(responseText);
-  } catch (error) {
-    console.error("Error parsing response body as JSON:", error);
-    throw error;
-  }
+    await client.connect();
+    console.log("Connected to MongoDB.");
 
-  return {
-    props: {
-      episodes,
-    },
-    revalidate: 60 * 60, // Regenerate the page every hour
-  };
+    const collection = client
+      .db(process.env.MONGODB_DB)
+      .collection("episodesGPT4");
+    console.log("Fetching episodes from MongoDB...");
+
+    let episodes = await collection.find().toArray();
+
+    episodes = JSON.parse(JSON.stringify(episodes));
+    console.log(`Fetched ${episodes.length} episodes.`);
+
+    return {
+      props: {
+        episodes,
+      },
+      revalidate: 60 * 60, // Regenerate the page every hour
+    };
+  } catch (error) {
+    console.error("Error fetching data from MongoDB:", error);
+    return {
+      props: {
+        episodes: [],
+        error: "Failed to fetch episodes",
+      },
+    };
+  } finally {
+    console.log("Closing connection to MongoDB...");
+    await client.close();
+    console.log("Closed connection to MongoDB.");
+  }
 }
