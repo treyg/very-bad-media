29003a7effc66cbdcf8c5f1b2a2468093c2fb445
Trey Gordon <41927921+treyg@users.noreply.github.com>
2024-11-28 12:27:14 -0500

    chore: remove vercel and plausible analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,15 +2,11 @@ import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
 import theme from "../theme/theme";
 import WithSubnavigation from "../components/WithSubNavigation";
 import Footer from "../components/Footer";
-import PlausibleProvider from "next-plausible";
+
 
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <PlausibleProvider
-        domain="yourdomain.com"
-        customDomain="https://plausible.tagweb.dev"
-      >
         <WithSubnavigation />
         <Flex direction="column" minH="100vh">
           <Container flex="1" maxW="8xl">
@@ -18,7 +14,6 @@ function App({ Component, pageProps }) {
           </Container>
           <Footer />
         </Flex>
-      </PlausibleProvider>
     </ChakraProvider>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,9 +10,23 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
@@ -28,7 +42,10 @@ class MyDocument extends Document {
             property="og:description"
             content="Media lists for books and movies discussed on Very Bad Wizards"
           />
-          <meta property="og:image" content="https://verybad.meda/public/hero.png" />
+          <meta
+            property="og:image"
+            content="https://verybad.meda/public/hero.png"
+          />
 
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="verybad.media" />
@@ -37,12 +54,19 @@ class MyDocument extends Document {
             property="twitter:description"
             content="Media lists for books and movies discussed on Very Bad Wizards"
           />
-          <meta property="twitter:image" content="https://verybad.meda/public/hero.png" />
+          <meta
+            property="twitter:image"
+            content="https://verybad.meda/public/hero.png"
+          />
 
           <link
             href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
             rel="stylesheet"
           />
+          <script
+            defer
+            src="https://umami.tagweb.dev/script.js"
+            data-website-id="6027c074-3fb6-458b-a0d0-10e548f0a7c0"></script>
         </Head>
         <body>
           <Main />
